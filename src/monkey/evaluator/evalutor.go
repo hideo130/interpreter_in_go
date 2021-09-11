06dc495a7dbc2b2fecc8abe2b9ba0405500924d1
Hideo Adachi <ckl15003@nitech.jp>
2021-09-11 23:53:59 +0900

    fisnish chapter 3

--- src/monkey/evaluator/evalutor.go
+++ src/monkey/evaluator/evalutor.go
@@ -16,6 +16,20 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	case *ast.BlockStatement:
 		return evalBlockStatement(node, env)
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
 	case *ast.IfExpression:
@@ -62,12 +76,54 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	return nil
 }
 
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a functon: %s", fn.Type())
+	}
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param:= range fn.Parameters{
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok{
+		return returnValue.Value
+	}
+	return obj
+}
+
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	} else {
 		return FALSE
 	}
+}
+
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	// Why is this function return array of object.Object
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
@@ -133,12 +189,11 @@ func isTruthy(obj object.Object) bool {
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
 	val, ok := env.Get(node.Value)
-	if!ok{
+	if !ok {
 		return newError("identifier not found: " + node.Value)
 	}
 	return val
 }
-
 
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
 	switch {
--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -1,21 +1,24 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
-
+	"monkey/ast"
+	"strings"
 )
 
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ = "ERROR"
+	ERROR_OBJ        = "ERROR"
+	Function_OBJ     = "FUNCTION"
 )
 
-type Error struct{
+type Error struct {
 	Message string
 }
 
@@ -42,10 +45,37 @@ func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value)
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType {
+	return Function_OBJ
+}
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+
 func (e *Error) Type() ObjectType {
 	return ERROR_OBJ
 }
-func (e *Error) Inspect()string {
+func (e *Error) Inspect() string {
 	return "ERROR: " + e.Message
 }
 
@@ -54,11 +84,11 @@ func (n *Null) Type() ObjectType {
 }
 func (n *Null) Inspect() string { return "null" }
 
-type ReturnValue struct{
+type ReturnValue struct {
 	Value Object
 }
 
-func (rv *ReturnValue)Type()ObjectType{return RETURN_VALUE_OBJ}
-func (rv *ReturnValue)Inspect()string  {
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string {
 	return rv.Value.Inspect()
 }
